5a1af6304e021f0ed5bfae96dbf2f1f9a10b5801
BENOITSLARVE98 <38895100+BENOITSLARVE98@users.noreply.github.com>
2023-09-12 17:19:41 -0400

    Initial Commit

---
+++ AI Challenge Tic Tac Toe/AI_Challenge_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Challenge_Tic_Tac_ToeApp.swift
+//  AI Challenge Tic Tac Toe
+//
+//  Created by Slarve N. on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_Challenge_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
